50a704ddb55d050fbcafb9da2c5269fd8d1743b0
renyabin <renyabin@zto.cn>
2018-11-21 21:48:53 +0800

    冒泡排序

---
+++ algorithm/src/main/java/com/ren/dream/algorithm/sort/Bubble.java
@@ -0,0 +1,37 @@
+package com.ren.dream.algorithm.sort;
+
+import java.util.Arrays;
+
+/**
+ * Created by 任亚彬 on 2018-11-21.
+ * 冒泡排序
+ */
+
+public class Bubble {
+  public static void main(String[] args) {
+    int[] test = new int[100];
+    for (int i = 0; i < test.length; i++) {
+      test[i] = (int) (Math.random() * 100);
+    }
+    System.out.println(Arrays.toString(test));
+
+    bubbleSort(test);
+    System.out.println(Arrays.toString(test));
+  }
+
+  private static void bubbleSort(int[] src) {
+    for (int i = 0; i < src.length - 1; i++) {
+      for (int j = src.length - 1; j > i; j--) {
+        if (src[j] < src[j - 1]) {
+          swap(src, j, j - 1);
+        }
+      }
+    }
+  }
+
+  private static void swap(int[] src, int i, int j) {
+    int temp = src[i];
+    src[i] = src[j];
+    src[j] = temp;
+  }
+}
